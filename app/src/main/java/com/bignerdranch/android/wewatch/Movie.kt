bee2d905189ff9262302e557407be92b72a12857
emil_ <emil.kushner.stud@mail.ru>
2024-02-26 22:49:25 +0700

    Описание сущности

--- app/src/main/java/com/bignerdranch/android/wewatch/Movie.kt
+++ app/src/main/java/com/bignerdranch/android/wewatch/Movie.kt
@@ -2,14 +2,16 @@ package com.bignerdranch.android.wewatch
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 import java.time.Year
 import java.util.Date
 import java.util.UUID
 
 @Entity
 class Movie (@PrimaryKey val id: UUID = UUID.randomUUID(),
-             var title: String = "",
-             var year: String = "",
-             var urlImage: String = "",
+             var Title: String = "",
+             var Year: String = "",
+             var Genres: String = "",
+             var Poster: String = "",
              var isWatched: Boolean = false){
 }
